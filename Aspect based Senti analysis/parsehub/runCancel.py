2f4c4dd8f1990cb3c85d6faa7fd51c7d88389f25
pranithkumar <praneeth970@gmail.com>
2017-12-22 17:40:40 +0530

    ParseHub Almost Done

--- Aspect based Senti analysis/parsehub/runCancel.py
+++ Aspect based Senti analysis/parsehub/runCancel.py
@@ -3,7 +3,7 @@ import requests
 params = {
   "api_key": "t3vujAq8hAiM"
 }
-r = requests.post("https://www.parsehub.com/api/v2/runs/tbB0mHjzPAEJ/cancel", data=params)
+r = requests.post("https://www.parsehub.com/api/v2/runs/tLX_DFcU2BvH/cancel", data=params)
 
 print(r.text)
 
