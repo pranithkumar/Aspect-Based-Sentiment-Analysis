5f3c4cf8fdb27a49120546c5d40a2147de7938b2
pranithkumar <praneeth970@gmail.com>
2017-12-22 17:41:50 +0530

    ParseHub Almost Done

--- Aspect based Senti analysis/parsehub/scrape.py
+++ Aspect based Senti analysis/parsehub/scrape.py
@@ -4,5 +4,5 @@ params = {
   "api_key": "t3vujAq8hAiM",
   "format": "json"
 }
-r = requests.get('https://www.parsehub.com/api/v2/runs/tbB0mHjzPAEJ/data', params=params)
+r = requests.get('https://www.parsehub.com/api/v2/runs/tLX_DFcU2BvH/data', params=params)
 print(r.text)
