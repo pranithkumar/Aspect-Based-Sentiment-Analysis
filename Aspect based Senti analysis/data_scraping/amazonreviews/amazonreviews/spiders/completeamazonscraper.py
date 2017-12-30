a20e45680d9144bd454be5ade92740e4498b5438
pranithkumar <praneeth970@gmail.com>
2017-12-30 10:18:20 +0530

    scraping completed

--- Aspect based Senti analysis/data_scraping/amazonreviews/amazonreviews/spiders/completeamazonscraper.py
+++ Aspect based Senti analysis/data_scraping/amazonreviews/amazonreviews/spiders/completeamazonscraper.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import html2text
 
 
 class CompleteamazonscraperSpider(scrapy.Spider):
@@ -13,21 +14,26 @@ class CompleteamazonscraperSpider(scrapy.Spider):
     if(int(link[1])/10<20):
         n=int(link[1])/10
     else:
-        n=20
+        n=10
     start_urls = ['http://amazon.in/'+link[0]+'&pageNumber={}'.format(i) for i in range(1,n)]
 
     def parse(self, response):
-        review = response.xpath('//span[contains(@class,"a-size-base review-text")]/text()').extract()
+        htmlreview = response.xpath('//span[contains(@class,"a-size-base review-text")]').extract()
         reviewer = response.xpath('//a[contains(@class,"a-size-base a-link-normal author")]/text()').extract()
         title = response.xpath('//a[contains(@class,"a-size-base a-link-normal review-title a-color-base a-text-bold")]/text()').extract()
         date = response.xpath('//span[contains(@class,"a-size-base a-color-secondary review-date")]/text()').extract()
         verification = response.xpath('//span[contains(@class,"a-size-mini a-color-state a-text-bold")]/text()').extract()
         rating = response.xpath('//span[contains(@class,"a-icon-alt")]/text()').extract()
+        converter = html2text.HTML2Text()
+        converter.ignore_links = True
+        review=[]
+        for i in range(0,len(htmlreview)):
+            review.append(converter.handle(htmlreview[i]))
         yield{
         	"reviewer":reviewer,
         	"title":title,
         	"rating":rating,
         	"date":date,
-            	"verification":verification,
+            "verification":verification,
         	"review":review
         }
