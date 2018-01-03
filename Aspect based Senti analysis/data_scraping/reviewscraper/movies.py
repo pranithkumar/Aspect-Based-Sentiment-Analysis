d33ea5954d301193a5e28b349cca97d1dfb2f483
pranithkumar <praneeth970@gmail.com>
2018-01-03 20:37:43 +0530

    data storage in separate files done

--- Aspect based Senti analysis/data_scraping/reviewscraper/movies.py
+++ Aspect based Senti analysis/data_scraping/reviewscraper/movies.py
@@ -22,12 +22,20 @@ def success(name):
     f = open('input.txt','w')
     f.write(name)
     f.close()
+    f = open('productlinkamazon.txt','r')
+    titlesamazon = f.read()
+    f.close()
+    titleamazon=titlesamazon.split('/')[1]
+    f = open('productlinkflipkart.txt','r')
+    titlesflipkart = f.read()
+    f.close()
+    titleflipkart=titlesflipkart.split('/')[1]
     os.system("scrapy crawl searchspiderflipkart")
-    os.system("scrapy crawl completeflipkartscraper -o flipkartreviews.json")
+    os.system("scrapy crawl completeflipkartscraper -o data/flipkart/"+titleflipkart+".json")
     os.system("scrapy crawl searchspideramazon")
     os.system("scrapy crawl getproductspideramazon")
-    os.system("scrapy crawl completeamazonscraper -o amazonreviews.json")
-    return render_template('review.html', AmazonReviews=json.load(open('amazonreviews.json')), FlipkartReviews=json.load(open('flipkartreviews.json')))
+    os.system("scrapy crawl completeamazonscraper -o data/amazon/"+titleamazon+".json")
+    return render_template('review.html', AmazonReviews=json.load(open("data/amazon/"+titleamazon+".json")), FlipkartReviews=json.load(open("data/flipkart/"+titleflipkart+".json")))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
