6f42bc5eff1b7e8f155107c92b482d0e90067e34
pjk23 <kalyanjithendra27@google.com>
2018-01-03 20:41:04 +0530

    nothing spcl

--- Aspect based Senti analysis/data_scraping/completereviews/test.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system("scrapy crawl fksearchspider")
-os.system("scrapy crawl completeflipkartscraper -o flipkartreviews.json")
-
-os.system("scrapy crawl searchspider")
-os.system("scrapy crawl getproductspider")
-os.system("scrapy crawl completeamazonscraper -o amazonreviews.json")
--- Aspect based Senti analysis/data_scraping/reviewscraper/test_sqlite.py
+++ Aspect based Senti analysis/data_scraping/reviewscraper/test_sqlite.py
@@ -1,12 +1,17 @@
 import sqlite3
 import json
 
-traffic = json.load(open('amazonreviews.json'))
+traffic = json.load(open('flipkartreviews.json'))
 conn = sqlite3.connect('database_test.db')
-
-someitem = traffic.itervalues().next()
-columns = list(someitem.keys())
-print columns
+conn.execute('CREATE TABLE mobiles1 (r_title TEXT, r_rating TEXT, r_review BLOB)')
+cur = conn.cursor()
+for review in traffic:
+    rev = review["review"]
+    rat = review["rating"]
+    tit = review["title"]
+    cur.execute("INSERT INTO mobiles1 (r_title,r_rating,) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+    con.commit()
+    #print key
 #conn.execute('CREATE TABLE mobiles (product TEXT, r_title TEXT, r_rating TEXT, r_review BLOB)')
 #print "table formed successfully"
 conn.close()
