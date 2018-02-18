c77a82ed0fdfec0f501e0513c5bb25551480f6fa
pjk23 <kalyanjithendra27@google.com>
2018-02-18 20:27:28 +0530

    alterations to code

--- Aspect based Senti analysis/aspect_categorization/aspect_catogerization.py
+++ Aspect based Senti analysis/aspect_categorization/aspect_catogerization.py
@@ -2,8 +2,8 @@ import pandas
 import nltk
 from nltk.tokenize import word_tokenize
 
-amazontext = pandas.read_json("~/Documents/Amazon_reviews_only.json")
-
+#amazontext = pandas.read_json("~/Documents/Amazon_reviews_only.json")
+amazontext = pandas.read_json("apple-iphone-6-space-grey-32-gb.json")
 def find_sub_list(sl,l):
 	result = []
 	sll=len(sl)
@@ -12,9 +12,9 @@ def find_sub_list(sl,l):
 			result.append((ind,ind+sll-1))
 	return result
 
-patterns = [['JJ','NN'],['JJ','NNS'],['JJ','NN','NN'],['JJ','NN','NNS'],['JJ','NNS','NN'],['JJ','NNS','NNS'],['RB','JJ'],['RBR','JJ'],['RBS','JJ'],['RB','JJ','NN'],['RBR','JJ','NN'],['RBS','JJ','NN'],['RB','JJ','NNS'],['RBR','JJ','NNS'],['RBS','JJ','NNS'],['RB','RB','NN'],['RBR','RB','NN'],['RBS','RB','NN'],['RB','RB','NNS'],['RBR','RB','NNS'],['RBS','RB','NNS'],['RB','RBR','NN'],['RBR','RBR','NN'],['RBS','RBR','NN'],['RB','RBR','NNS'],['RBR','RBR','NNS'],['RBS','RBR','NNS'],['RB','RBS','NN'],['RBR','RBS','NN'],['RBS','RBS','NN'],['RB','RBS','NNS'],['RBR','RBS','NNS'],['RBS','RBS','NNS'],['RB','VBN'],['RBR','VBN'],['RBS','VBN'],['RB','VBD'],['RBR','VBD'],['RBS','VBD'],['VBN','NN'],['VBN','NNS'],['VBD','NN'],['VBD','NNS'],['VBN','RB'],['VBN','RBR'],['VBN','RBS'],['VBD','RB'],['VBD','RBR'],['VBD','RBS']]
-
-for review in amazontext['reviewText']:
+patterns = [['JJ','NN','NN'],['JJ','NN','NNS'],['JJ','NNS','NN'],['JJ','NNS','NNS'],['RB','JJ','NN'],['RBR','JJ','NN'],['RBS','JJ','NN'],['RB','JJ','NNS'],['RBR','JJ','NNS'],['RBS','JJ','NNS'],['RB','RB','NN'],['RBR','RB','NN'],['RBS','RB','NN'],['RB','RB','NNS'],['RBR','RB','NNS'],['RBS','RB','NNS'],['RB','RBR','NN'],['RBR','RBR','NN'],['RBS','RBR','NN'],['RB','RBR','NNS'],['RBR','RBR','NNS'],['RBS','RBR','NNS'],['RB','RBS','NN'],['RBR','RBS','NN'],['RBS','RBS','NN'],['RB','RBS','NNS'],['RBR','RBS','NNS'],['RBS','RBS','NNS'],['JJ','NN'],['JJ','NNS'],['RB','JJ'],['RBR','JJ'],['RBS','JJ'],['RB','VBN'],['RBR','VBN'],['RBS','VBN'],['RB','VBD'],['RBR','VBD'],['RBS','VBD'],['VBN','NN'],['VBN','NNS'],['VBD','NN'],['VBD','NNS'],['VBN','RB'],['VBN','RBR'],['VBN','RBS'],['VBD','RB'],['VBD','RBR'],['VBD','RBS']]
+'''
+for review in amazontext['review']:
 	print "\n"+review
 	word_tokens = word_tokenize(review)
 	pos_tuples = nltk.pos_tag(word_tokens)
@@ -31,3 +31,24 @@ for review in amazontext['reviewText']:
 				#print indices
 				for i in range(int(indices[0]),int(indices[1])+1):
 					print pos_tuples[i]
+'''
+for review in amazontext['review']:
+	print "\n"+review
+	lines = []
+	lines = review.split('.')
+	for line in lines:
+		word_tokens = word_tokenize(line)
+		pos_tuples = nltk.pos_tag(word_tokens)
+		print pos_tuples
+		pos =[]
+		for tag in pos_tuples:
+			pos.append(tag[1])
+		for pattern in patterns:
+			if find_sub_list(pattern,pos) != None:
+				parts = find_sub_list(pattern,pos)
+				for indices in parts:
+					#indices = find_sub_list(pattern,pos)
+					print pattern
+					#print indices
+					for i in range(int(indices[0]),int(indices[1])+1):
+						print pos_tuples[i]
--- Aspect based Senti analysis/aspect_categorization/rakefinal.py
+++ Aspect based Senti analysis/aspect_categorization/rakefinal.py
@@ -24,25 +24,28 @@ def find_sub_list(sl,l):
 
 f = open("testtext.txt","r")
 
-patterns = [['JJ','NN'],['JJ','NNS'],['JJ','NN','NN'],['JJ','NN','NNS'],['JJ','NNS','NN'],['JJ','NNS','NNS'],['RB','JJ'],['RBR','JJ'],['RBS','JJ'],['RB','JJ','NN'],['RBR','JJ','NN'],['RBS','JJ','NN'],['RB','JJ','NNS'],['RBR','JJ','NNS'],['RBS','JJ','NNS'],['RB','RB','NN'],['RBR','RB','NN'],['RBS','RB','NN'],['RB','RB','NNS'],['RBR','RB','NNS'],['RBS','RB','NNS'],['RB','RBR','NN'],['RBR','RBR','NN'],['RBS','RBR','NN'],['RB','RBR','NNS'],['RBR','RBR','NNS'],['RBS','RBR','NNS'],['RB','RBS','NN'],['RBR','RBS','NN'],['RBS','RBS','NN'],['RB','RBS','NNS'],['RBR','RBS','NNS'],['RBS','RBS','NNS'],['RB','VBN'],['RBR','VBN'],['RBS','VBN'],['RB','VBD'],['RBR','VBD'],['RBS','VBD'],['VBN','NN'],['VBN','NNS'],['VBD','NN'],['VBD','NNS'],['VBN','RB'],['VBN','RBR'],['VBN','RBS'],['VBD','RB'],['VBD','RBR'],['VBD','RBS']]
+patterns = [['JJ','NN','NN'],['JJ','NN','NNS'],['JJ','NNS','NN'],['JJ','NNS','NNS'],['RB','JJ','NN'],['RBR','JJ','NN'],['RBS','JJ','NN'],['RB','JJ','NNS'],['RBR','JJ','NNS'],['RBS','JJ','NNS'],['RB','RB','NN'],['RBR','RB','NN'],['RBS','RB','NN'],['RB','RB','NNS'],['RBR','RB','NNS'],['RBS','RB','NNS'],['RB','RBR','NN'],['RBR','RBR','NN'],['RBS','RBR','NN'],['RB','RBR','NNS'],['RBR','RBR','NNS'],['RBS','RBR','NNS'],['RB','RBS','NN'],['RBR','RBS','NN'],['RBS','RBS','NN'],['RB','RBS','NNS'],['RBR','RBS','NNS'],['RBS','RBS','NNS'],['JJ','NN'],['JJ','NNS'],['RB','JJ'],['RBR','JJ'],['RBS','JJ'],['RB','VBN'],['RBR','VBN'],['RBS','VBN'],['RB','VBD'],['RBR','VBD'],['RBS','VBD'],['VBN','NN'],['VBN','NNS'],['VBD','NN'],['VBD','NNS'],['VBN','RB'],['VBN','RBR'],['VBN','RBS'],['VBD','RB'],['VBD','RBR'],['VBD','RBS']]
 
 for review in f.readlines():
 	print "\n"+review
-	word_tokens = word_tokenize(review)
-	pos_tuples = nltk.pos_tag(word_tokens)
-	#print pos_tuples
-	pos =[]
-	for tag in pos_tuples:
-		pos.append(tag[1])
-	for pattern in patterns:
-		if find_sub_list(pattern,pos) != None:
-			parts = find_sub_list(pattern,pos)
-			for indices in parts:
-				#indices = find_sub_list(pattern,pos)
-				print pattern
-				#print indices
-				for i in range(int(indices[0]),int(indices[1])+1):
-					print pos_tuples[i]
+	lines = []
+	lines = review.split('.')
+	for line in lines:
+		word_tokens = word_tokenize(line)
+		pos_tuples = nltk.pos_tag(word_tokens)
+		#print pos_tuples
+		pos =[]
+		for tag in pos_tuples:
+			pos.append(tag[1])
+		for pattern in patterns:
+			if find_sub_list(pattern,pos) != None:
+				parts = find_sub_list(pattern,pos)
+				for indices in parts:
+					#indices = find_sub_list(pattern,pos)
+					print pattern
+					#print indices
+					for i in range(int(indices[0]),int(indices[1])+1):
+						print pos_tuples[i]
 	'''for pattern in patterns:
 		if sublist(pattern,pos_tuples) != 0:
 			print sublist(pattern,pos_tuples)'''
