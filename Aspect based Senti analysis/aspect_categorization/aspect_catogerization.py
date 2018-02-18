85c8e56078ce5fab49e5edd71bbf600f0dc8f09a
pranithkumar <praneeth970@gmail.com>
2018-02-18 15:43:16 +0530

    aspect cat going on

--- Aspect based Senti analysis/aspect_categorization/aspect_catogerization.py
+++ Aspect based Senti analysis/aspect_categorization/aspect_catogerization.py
@@ -1,18 +1,8 @@
+import pandas
 import nltk
 from nltk.tokenize import word_tokenize
 
-'''def sublist(lst1, lst2):
-	pos = []
-	tokens = []
-	for tag in lst2:
-		pos.append(tag[1])
-		tokens.append(tag[0])
-	ls1 = [element for element in lst1 if element in pos]
-	ls2 = [element for element in pos if element in lst1]
-	if ls1 == ls2:
-		return ls2
-	else:
-		return 0'''
+amazontext = pandas.read_json("~/Documents/Amazon_reviews_only.json")
 
 def find_sub_list(sl,l):
 	result = []
@@ -22,11 +12,9 @@ def find_sub_list(sl,l):
 			result.append((ind,ind+sll-1))
 	return result
 
-f = open("testtext.txt","r")
-
 patterns = [['JJ','NN'],['JJ','NNS'],['JJ','NN','NN'],['JJ','NN','NNS'],['JJ','NNS','NN'],['JJ','NNS','NNS'],['RB','JJ'],['RBR','JJ'],['RBS','JJ'],['RB','JJ','NN'],['RBR','JJ','NN'],['RBS','JJ','NN'],['RB','JJ','NNS'],['RBR','JJ','NNS'],['RBS','JJ','NNS'],['RB','RB','NN'],['RBR','RB','NN'],['RBS','RB','NN'],['RB','RB','NNS'],['RBR','RB','NNS'],['RBS','RB','NNS'],['RB','RBR','NN'],['RBR','RBR','NN'],['RBS','RBR','NN'],['RB','RBR','NNS'],['RBR','RBR','NNS'],['RBS','RBR','NNS'],['RB','RBS','NN'],['RBR','RBS','NN'],['RBS','RBS','NN'],['RB','RBS','NNS'],['RBR','RBS','NNS'],['RBS','RBS','NNS'],['RB','VBN'],['RBR','VBN'],['RBS','VBN'],['RB','VBD'],['RBR','VBD'],['RBS','VBD'],['VBN','NN'],['VBN','NNS'],['VBD','NN'],['VBD','NNS'],['VBN','RB'],['VBN','RBR'],['VBN','RBS'],['VBD','RB'],['VBD','RBR'],['VBD','RBS']]
 
-for review in f.readlines():
+for review in amazontext['reviewText']:
 	print "\n"+review
 	word_tokens = word_tokenize(review)
 	pos_tuples = nltk.pos_tag(word_tokens)
@@ -43,7 +31,3 @@ for review in f.readlines():
 				#print indices
 				for i in range(int(indices[0]),int(indices[1])+1):
 					print pos_tuples[i]
-	'''for pattern in patterns:
-		if sublist(pattern,pos_tuples) != 0:
-			print sublist(pattern,pos_tuples)'''
-	#print pos
