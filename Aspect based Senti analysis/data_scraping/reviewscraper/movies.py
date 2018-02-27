907be8b1e64e507060eb89278b86dc324aed3547
Ubuntu <ubuntu@ip-172-31-42-206.us-east-2.compute.internal>
2018-02-27 10:06:01 +0000

    scraping aws done

--- Aspect based Senti analysis/data_scraping/reviewscraper/movies.py
+++ Aspect based Senti analysis/data_scraping/reviewscraper/movies.py
@@ -29,40 +29,40 @@ def success(name):
     f = open('/home/ubuntu/Aspect-Based-Sentiment-Analysis/Aspect based Senti analysis/data_scraping/reviewscraper/input.txt','w')
     f.write(name)
     f.close()
-    print "step 1"
+#    print "step 1"
     os.chdir('/home/ubuntu/Aspect-Based-Sentiment-Analysis/Aspect based Senti analysis/data_scraping/reviewscraper/')
-    print "step 2"
+#    print "step 2"
     os.system("scrapy crawl searchspiderflipkart")
-    print "step 3"
+#    print "step 3"
     f = open('/home/ubuntu/Aspect-Based-Sentiment-Analysis/Aspect based Senti analysis/data_scraping/reviewscraper/productlinkflipkart.txt','r')
-    print "step 4"
+#    print "step 4"
     #extracting filename of the json file to be stored from productlinkflipkart.txt
     filenameflipkart = f.read()
     f.close()
     fileflipkart=filenameflipkart.split('/')[1]
     #check if the file already exists
-#    if os.path.exists("/home/ubuntu/Aspect-Based-Sentiment-Analysis/Aspect based Senti analysis/data_scraping/reviewscraper/data/flipkart/"+fileflipkart+".json"):
-#        os.system("rm /home/ubuntu/Aspect-Based-Sentiment-Analysis/Aspect based Senti analysis/data_scraping/reviewscraper/data/flipkart/"+fileflipkart+".json")
-    print "step 5"
+    if os.path.exists("data/flipkart/"+fileflipkart+".json"):
+        os.system("rm data/flipkart/"+fileflipkart+".json")
+#    print "step 5"
     os.system("scrapy crawl completeflipkartscraper -o data/flipkart/"+fileflipkart+".json")
-    print "step 6"
+#    print "step 6"
     os.system("scrapy crawl searchspideramazon")
-    print "step 7"
+#    print "step 7"
     os.system("scrapy crawl getproductspideramazon")
-    print "step 8"
+#    print "step 8"
     f = open('/home/ubuntu/Aspect-Based-Sentiment-Analysis/Aspect based Senti analysis/data_scraping/reviewscraper/productlinkamazon.txt','r')
     #extracting filename of the json file to be stored from productlinkamazon.txt
-    print "step 9"
+#    print "step 9"
     filenameamazon = f.read()
     f.close()
     fileamazon=filenameamazon.split('/')[1]
     #check if the file already exists
-#    if os.path.exists("/home/ubuntu/Aspect-Based-Sentiment-Analysis/Aspect based Senti analysis/data_scraping/reviewscraper/data/amazon/"+fileamazon+".json"):
-#        os.system("rm /home/ubuntu/Aspect-Based-Sentiment-Analysis/Aspect based Senti analysis/data_scraping/reviewscraper/data/amazon/"+fileamazon+".json")
-    print "step 10"
+    if os.path.exists("data/amazon/"+fileamazon+".json"):
+        os.system("rm data/amazon/"+fileamazon+".json")
+#    print "step 10"
     os.system("scrapy crawl completeamazonscraper -o data/amazon/"+fileamazon+".json")
     #rendering data from files to the html output
-    print "step 11"
+#    print "step 11"
     return render_template('review.html', AmazonReviews=json.load(open("/home/ubuntu/Aspect-Based-Sentiment-Analysis/Aspect based Senti analysis/data_scraping/reviewscraper/data/amazon/"+fileamazon+".json")), FlipkartReviews=json.load(open("/home/ubuntu/Aspect-Based-Sentiment-Analysis/Aspect based Senti analysis/data_scraping/reviewscraper/data/flipkart/"+fileflipkart+".json")))
 
 if __name__ == '__main__':
