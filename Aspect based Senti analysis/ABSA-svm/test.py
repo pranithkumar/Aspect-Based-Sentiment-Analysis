76672f569a404070ddb2c921a2d1fc8e988ec2da
pranithkumar <praneeth970@gmail.com>
2018-03-05 15:40:47 +0530

    changes

--- Aspect based Senti analysis/ABSA-svm/test.py
+++
@@ -1,5 +0,0 @@
-from sklearn.externals import joblib
-
-tagged_text_list_train=joblib.load('tagged_text_list_train.pkl')
-file = open('testfile.txt','w') 
-file.write(tagged_text_list_train)
--- Aspect based Senti analysis/ABSA-svm/test1.py
+++
@@ -1,4 +0,0 @@
-from sklearn.externals import joblib
-
-tagged_text_list_train=joblib.load('test_dump')
-joblib.dump(tagged_text_list_train,'tagged_text_list_train_final.pkl')
