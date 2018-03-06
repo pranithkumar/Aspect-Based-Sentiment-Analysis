4d872b4cddeca6ec9e061708f0b066d8393fc62c
pranithkumar <praneeth970@gmail.com>
2018-03-06 15:15:41 +0530

    aspect entity dictionary done

--- Aspect based Senti analysis/aspect_categorization/aspect_catogerization.py
+++ Aspect based Senti analysis/aspect_categorization/aspect_catogerization.py
@@ -1,3 +1,4 @@
+import pprint
 import pandas
 import nltk
 from nltk.tokenize import word_tokenize
@@ -15,8 +16,10 @@ def find_sub_list(sl,l):
 patterns1 = [['NN','NN','VBN','JJ'],['NN','NNS','VBN','JJ'],['NNS','NN','VBN','JJ'],['NNS','NNS','VBN','JJ'],['NN','NN','VBD','JJ'],['NN','NNS','VBD','JJ'],['NNS','NN','VBD','JJ'],['NNS','NNS','VBD','JJ'],['JJ','NN','NN'],['JJ','NN','NNS'],['JJ','NNS','NN'],['JJ','NNS','NNS'],['RB','JJ','NN'],['RBR','JJ','NN'],['RBS','JJ','NN'],['RB','JJ','NNS'],['RBR','JJ','NNS'],['RBS','JJ','NNS'],['RB','RB','NN'],['RBR','RB','NN'],['RBS','RB','NN'],['RB','RB','NNS'],['RBR','RB','NNS'],['RBS','RB','NNS'],['RB','RBR','NN'],['RBR','RBR','NN'],['RBS','RBR','NN'],['RB','RBR','NNS'],['RBR','RBR','NNS'],['RBS','RBR','NNS'],['RB','RBS','NN'],['RBR','RBS','NN'],['RBS','RBS','NN'],['RB','RBS','NNS'],['RBR','RBS','NNS'],['RBS','RBS','NNS'],['NN','VBZ','JJ'],['NNP','NN'],['JJ','NN'],['JJ','NNS'],['VBN','NN'],['VBN','NNS'],['VBD','NN'],['VBD','NNS']]
 
 patterns2 = [['RB','JJ'],['RBR','JJ'],['RBS','JJ'],['RB','VBN'],['RBR','VBN'],['RBS','VBN'],['RB','VBD'],['RBR','VBD'],['RBS','VBD'],['VBN','RB'],['VBN','RBR'],['VBN','RBS'],['VBD','RB'],['VBD','RBR'],['VBD','RBS']]
-
+aspects_dict = {}
 for review in amazontext['review']:
+	nounlist = ['NN','NNS']
+	#aspects_dict = {}
 	print "\n"+review
 	lines = []
 	lines = review.split('.')
@@ -34,12 +37,26 @@ for review in amazontext['review']:
 					#indices = find_sub_list(pattern,pos)
 					print pattern
 					#print indices
+					aspect = '1 '
+					entity = '2 '
 					for i in range(int(indices[0]),int(indices[1])+1):
+						if pos_tuples[i][1] in nounlist:
+							aspect = aspect + ' ' + pos_tuples[i][0]
+						else:
+							entity = entity + ' ' + pos_tuples[i][0]
 						print pos_tuples[i]
+					if aspect in aspects_dict:
+						if entity in aspects_dict[aspect]:
+							aspects_dict[aspect][entity] += 1
+						else:
+							aspects_dict[aspect][entity] = 1
+					else:
+						aspects_dict[aspect] = {}
+						aspects_dict[aspect][entity] = 1
+		
 		for pattern in patterns2:
 			if find_sub_list(pattern,pos) != None:
 				parts = find_sub_list(pattern,pos)
-				nounlist = ['NN','NNS','NNP']
 				check_noun = {}
 				for indices in parts:
 					i = 0
@@ -58,5 +75,20 @@ for review in amazontext['review']:
 					if check_noun:
 						print pos_tuples[noun_index[0]]
 					#print indices
+					aspect = '3 '
+					entity = '4 '
+					if check_noun:
+						aspect = pos_tuples[noun_index[0]][0]
 					for i in range(int(indices[0]),int(indices[1])+1):
+						entity = entity + ' ' + pos_tuples[i][0]
 						print pos_tuples[i]
+					if aspect in aspects_dict:
+						if entity in aspects_dict[aspect]:
+							aspects_dict[aspect][entity] += 1
+						else:
+							aspects_dict[aspect][entity] = 1
+					else:
+						aspects_dict[aspect] = {}
+						aspects_dict[aspect][entity] = 1
+
+pprint.pprint(aspects_dict)
