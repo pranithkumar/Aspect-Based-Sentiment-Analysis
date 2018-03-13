f7b3ce32317138377a3f38a2982b1d0bb5b51898
pranithkumar <praneeth970@gmail.com>
2018-03-13 21:06:58 +0530

    aspects

---
+++ Aspect based Senti analysis/aspect_categorization/final_aspect_entity_extraction.py
@@ -0,0 +1,126 @@
+import datapreprocessing as dp
+from extract_aspects_mine import *
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import nltk,pandas,re,pprint,sys
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+def aspects_from_tagged_sents(tagged_sentences):
+	"""
+	INPUT: list of lists of strings
+	OUTPUT: list of aspects
+
+	Given a list of tokenized and pos_tagged sentences from reviews
+	about a given restaurant, return the most common aspects
+	"""
+
+	STOPWORDS = set(stopwords.words('english'))
+
+	# find the most common nouns in the sentences
+	asp_ent_pair = {}
+	c = 0
+
+	for sent in tagged_sentences:
+		if sent[1]=='NNP' or sent[1]=='NN' or sent[1]=='NNS' and sent[0] not in STOPWORDS:
+			asp_ent_pair[sent[0]] = {}
+			c = 1
+
+	for sent in tagged_sentences:
+		if c == 0:
+			break
+		if sent[1] in ['JJ','RB','RBR','RBS','VBN','VBD','VBP'] and sent[0] not in STOPWORDS:
+			for asp in asp_ent_pair:
+				asp_ent_pair[asp][sent[0]] = 1
+
+	# list of tuples of form (noun, count)
+	return asp_ent_pair
+
+
+#Using preprocessed text for categorization
+reviews = dp.dataframecomplete('Apple-iPhone-Space-Grey-32GB.json','apple-iphone-6-space-grey-32-gb.json')
+
+#initnalizing the aspects dictionary
+aspects_dict = {}
+
+#if reviews are taken after preprocessing use below line for looping
+for review in reviews['review']:
+
+	#converting all reviews to lowercase
+	review = review.lower()
+	res = ' '
+	for ch in review:
+		if not re.match('[a-zA-Z0-9_\' ]',ch):
+			res = res + ' ' + ch + ' '
+		else:
+			res = res + ch
+	review = res[1:]
+	print "\n\n\nreview: "+review
+
+	word_tokens = word_tokenize(review)
+	#pos tagging
+	pos_tuples = nltk.pos_tag(word_tokens)
+
+	split_sets = []
+	split_list = ' '
+	final_sets = []
+	split_words = [',','.',';','!','?']
+
+	for word,pos in pos_tuples:
+		if pos == 'CC':
+			split_words.append(word)
+	
+	#print "word tokens:"
+	#print word_tokens
+	#print "pos tuples:"
+	#print pos_tuples
+
+	for word in word_tokens:
+		if word in split_words:
+			split_sets.append(split_list[2:])
+			split_list = ' '
+		if word not in split_words:
+			split_list += ' ' + word
+	split_sets.append(split_list[2:])
+
+	for i in range(0,len(split_sets)):
+		if len(split_sets[i])!=0:
+			final_sets.append(split_sets[i])
+
+	split_words = [',','.',';']
+
+	print "sets:"
+	print final_sets
+
+	for sentence in final_sets:
+		tokenized_data = tokenize(sentence.encode('utf-8','ignore'))
+
+		#print "tokenized_data:"
+		#print tokenized_data
+		
+		pos_tagged_data = pos_tag(tokenized_data)
+		
+		#print "pos_tagged_data:"
+		#print pos_tagged_data
+		
+		final_aspects = {}
+		
+		aspects_data = aspects_from_tagged_sents(pos_tagged_data)
+
+		print "aspects and entities :"
+		print aspects_data
+
+		'''if aspects_data[0][0] in aspects_dict:
+			if ent in aspects_dict[aspects_data[0][0]]:
+				aspects_dict[aspects_data[0][0]][aspects_data[1][0]] += 1
+			else:
+				aspects_dict[data[0][0]][aspects_data[1][0]] = 0
+		else:
+			aspects_dict[aspects_data[0][0]] = {}
+			aspects_dict[aspects_data[0][0]][aspects_data[1][0]] = 0
+		
+		print aspects_dict'''
+
+'''for key, value in sorted(aspects_dict.iteritems(), key=lambda (k,v): (v,k),reverse = True):
+    print "%s: %s" % (key, value)'''
---
+++ Aspect based Senti analysis/aspect_categorization/main.py
@@ -0,0 +1,102 @@
+from __future__ import division
+import datapreprocessing as dp
+import pandas as pd
+import numpy as np
+
+def get_reviews_for_business(bus_id, df):
+	"""
+	INPUT: business id, pandas DataFrame
+	OUTPUT: Series with only texts
+	
+	For a given business id, return the review_id and 
+	text of all reviews for that business. 
+	"""
+	return df.text[df.business_id==bus_id]
+
+
+def extract_aspects(reviews):
+	"""
+	INPUT: iterable of strings (pd Series, list)
+	OUTPUT: list of aspects
+	
+	Return the aspects from the set of reviews
+	"""
+
+	# import the aspect extraction functions
+	from extract_aspects import get_sentences, tokenize, pos_tag, aspects_from_tagged_sents
+
+	# put all the sentences in all reviews in one stream
+	sentences = []
+	for review in reviews: 
+		sentences.extend(get_sentences(review.encode('utf-8','ignore')))
+
+	tokenized_sentences = [tokenize(sentence) for sentence in sentences
+							for sentences in get_sentences(review.encode('utf-8','ignore'))]
+
+	# tokenize each sentence
+	#tokenized_sentences = [tokenize(sentence) for sentence in sentences]
+
+	# pos tag each sentence
+	tagged_sentences = [pos_tag(sentence) for sentence in tokenized_sentences]
+
+	# from the pos tagged sentences, get a list of aspects
+	aspects = aspects_from_tagged_sents(tagged_sentences)
+
+	return aspects
+
+
+def score_aspect(reviews, aspect):
+	"""
+	INPUT: iterable of reviews, iterable of aspects
+	OUTPUT: score of aspect on given set of reviews
+	
+	For a set of reviews and corresponding aspects, 
+	return the score of the aspect on the reviews
+	"""
+
+	from score_aspect import SentimentScorer, get_sentences_by_aspect
+
+	sentiment_scorer = SentimentScorer()
+	aspect_sentences = get_sentences_by_aspect(aspect, reviews)
+	scores = [sentiment_scorer.score(sent) for sent in aspect_sentences]
+	
+	print scores
+	return np.mean(scores)
+
+
+def aspect_opinions(reviews):
+	"""
+	INPUT: a set of reviews
+	OUTPUT: dictionary with aspects as keys and values as scores
+	"""
+
+	aspects = extract_aspects(reviews)
+	return dict([(aspect, score_aspect(reviews, aspect)) for aspect in aspects])
+
+
+def read_data():
+	"""
+	INPUT: None
+	OUTPUT: pandas data frame from file
+	"""
+	return pd.read_csv('high_review_restaurants.csv')
+
+
+def main(): 
+	"""
+	The true main. 
+	"""
+
+	reviews = dp.dataframecomplete('Apple-iPhone-Space-Grey-32GB.json','apple-iphone-6-space-grey-32-gb.json')
+	score = {}
+
+	score[business] = aspect_opinions(reviews.encode('utf-8','ignore'))
+	print score
+
+if __name__ == "__main__":
+	
+	reviews = dp.dataframecomplete('Apple-iPhone-Space-Grey-32GB.json','apple-iphone-6-space-grey-32-gb.json')
+	score = {}
+
+	score[business] = aspect_opinions(reviews)
+	print score
